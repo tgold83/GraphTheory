30ef7c5be5c6b842657c198e368dff11a6debe1a
Ted Gold <ted.jonathan.gold@gmail.com>
2023-03-07 13:43:57 -0800

    add passing test 2

--- _tests_/graph.test.js
+++ _tests_/graph.test.js
@@ -11,4 +11,9 @@ describe('Graph', () => {
   test('should correctly instantiate a graph', () => {
     expect(graph.adjacencyList.size).toEqual(0);
   });
+
+  test('should add a new node', () => {
+    graph.addNode("Jasmine");
+    expect(graph.adjacencyList.get("Jasmine").size).toEqual(0);
+  });
 });
--- src/graph.js
+++ src/graph.js
@@ -2,4 +2,7 @@ export default class Graph {
   constructor() {
     this.adjacencyList = new Map();
   }
+  addNode(name) {
+    this.adjacencyList.set(name, new Set());
+  }
 }
